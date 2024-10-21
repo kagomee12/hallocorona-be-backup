a1b348123ebf9e6fc9580d6c8d52f42d4134b8c0
kagomee12 <kalfitrah@gmail.com>
2024-10-21 15:57:50 +0700

    fixing bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
-import { AuthModule } from './auth/auth.module';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { UsersModule } from './users/users.module';
+import { ClodinaryModule } from './clodinary/clodinary.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ClodinaryModule, 
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })

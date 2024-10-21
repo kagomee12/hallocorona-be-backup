3bfb90d7ba48f70f1c387e2f9059efe96b6020a6
kagomee12 <kalfitrah@gmail.com>
2024-10-21 16:09:35 +0700

    add package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from '../src/prisma/prisma.service';
+import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 import { ClodinaryModule } from './clodinary/clodinary.module';
 
 @Module({
   imports: [
-    ClodinaryModule, 
+    ClodinaryModule,
     UsersModule
   ],
   controllers: [AppController],

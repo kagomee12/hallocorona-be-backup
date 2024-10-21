de150e8d5ab55d9fae171977e1843a8c14e4acf8
kagomee12 <kalfitrah@gmail.com>
2024-10-21 16:25:02 +0700

    add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 import { ClodinaryModule } from './clodinary/clodinary.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ClodinaryModule,
-    UsersModule
+    UsersModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
